740e231c912610fc0bcb28161f562b3d275ad709
Oliver Kiss <oliverkyss@gmail.com>
2021-08-25 23:59:24 +0200

    LocationInput: show/hide suggestions

--- src/components/LocationInput.svelte
+++ src/components/LocationInput.svelte
@@ -5,7 +5,9 @@
 
   export let value = '';
 
+  let input: HTMLInputElement;
   let suggestions: { name: string; id: string }[] = [];
+  let suggestionsVisiblity = false;
 
   const searchLocation = async (e: any) => {
     const searchText: string = e.target.value;
@@ -25,12 +27,29 @@
   const selectSuggestion = (index: number) => async () => {
     const details = await getTimezone(suggestions[index].id);
     console.log(details);
+    value = suggestions[index].name;
+    suggestionsVisiblity = false;
+    input.blur();
   };
 </script>
 
-<input type="text" {value} on:input={debounce(searchLocation)} on:change={selectSuggestion(0)} />
-<ul>
-  {#each suggestions as { name }, index}
-    <li on:click={selectSuggestion(index)}>{name}</li>
-  {/each}
-</ul>
+<div>
+  <div>
+    <input
+      type="text"
+      {value}
+      bind:this={input}
+      on:input={debounce(searchLocation)}
+      on:change={selectSuggestion(0)}
+      on:focus={() => (suggestionsVisiblity = true)}
+    />
+    <button on:click={selectSuggestion(0)} disabled={suggestions.length === 0}>Set</button>
+  </div>
+  <div class:suggestionsVisible={suggestionsVisiblity}>
+    <ul>
+      {#each suggestions as { name, id }, index (id)}
+        <li on:click={selectSuggestion(index)}>{name}</li>
+      {/each}
+    </ul>
+  </div>
+</div>
