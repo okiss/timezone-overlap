bf7e4a2f49330842a58d77e7bc9f353fca1a1f8c
Oliver Kiss <oliverkyss@gmail.com>
2021-09-17 22:51:53 +0200

    remove global.css

--- src/App.svelte
+++ src/App.svelte
@@ -36,6 +36,48 @@
 </main>
 
 <style>
+  :global(html, body) {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+    line-height: 1;
+  }
+
+  :global(body) {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif;
+    min-width: 1000px;
+  }
+
+  :global(html) {
+    box-sizing: border-box;
+  }
+  :global(*, *:before, *:after) {
+    box-sizing: inherit;
+  }
+
+  :global(:root) {
+    --background: white;
+    --primary: #18244f;
+
+    --spacing-1: 4px;
+    --spacing-2: 8px;
+    --spacing-3: 12px;
+    --spacing-4: 20px;
+    --spacing-5: 32px;
+    --spacing-6: 52px;
+    --spacing-7: 84px;
+
+    --radius-1: 6px;
+    --radius-2: 8px;
+
+    --time-slot-cell-size: 40px;
+
+    --font-monospace: Consolas, monaco, monospace;
+  }
+
   header {
     display: flex;
     align-items: center;
